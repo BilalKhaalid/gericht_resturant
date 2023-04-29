0554593fa424e9c9cf343752a8c878388433cdb3
BilalKhaalid <bilall1432@gmail.com>
2023-04-29 14:46:29 +0500

    Navbar Completed

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-
-import './Header.css';
+import React from "react";
+import images from "../../constants/images";
+import { SubHeading } from "../../components";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title={"Chase The New Flavor"} />
+      <h1 className="app__header-h1">The Key To Fine Dinning</h1>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
