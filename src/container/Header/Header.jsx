80f64b5dc48d784fe63949a0bf95d7b4ac77c396
BilalKhaalid <bilall1432@gmail.com>
2023-04-29 15:04:57 +0500

    Header completed here!

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,8 +8,17 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title={"Chase The New Flavor"} />
       <h1 className="app__header-h1">The Key To Fine Dinning</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nulla
+        exercitationem harum.
+      </p>
+      <button className="custom__button" type="button">
+        Explore Menu
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome" />
+    </div>
   </div>
 );
 
