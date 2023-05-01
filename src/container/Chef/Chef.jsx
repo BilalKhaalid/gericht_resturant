b07d6af5e1b9c7615b72cc79d6cec3679d764562
BilalKhaalid <bilall1432@gmail.com>
2023-05-01 10:33:46 +0500

    Chef section completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+            consequatur nostrum voluptate!
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
+          ratione optio non? Sint ab fugiat distinctio quia necessitatibus vel
+          explicabo rem, cum nesciunt enim excepturi.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
