e4b8aae574af2383bbf3b114d65e45aed7603336
BilalKhaalid <bilall1432@gmail.com>
2023-05-01 11:13:34 +0500

    Intro section completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -4,7 +4,7 @@ import { SubHeading } from "../../components";
 import "./Chef.css";
 
 const Chef = () => (
-  <div className="app__bg app__wrapper section__padding">
+  <div className="app__bg app__wrapper section__padding" >
     <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="chef" />
     </div>
