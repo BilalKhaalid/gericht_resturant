9aa60ccf4de2de6171b8d3cd12ac8e0ace9cbdca
BilalKhaalid <bilall1432@gmail.com>
2023-05-03 11:25:30 +0500

    footer completed

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
-
-import './Footer.css';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, saepe.
+        </p>
+        <p className="p__opensans">+1 240-68820 </p>
+        <p className="p__opensans">+1 240-68820 </p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="gericht" />
+        <p className="p__opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others."
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: 50 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">10:00 am - 02:00 am </p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">10:00 am - 12:00 am</p>
+      </div>
+    </div>
+    <div className="app__footer-copyright">
+      <p className="p__opensans">2023 Gericht. All Rights Reserved</p>
+    </div>
   </div>
 );
 
